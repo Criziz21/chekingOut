4b83fc4f2b144dbe7b59f1e7d7007f1d10521bd8
Criziz21 <xioamialisher777@gmail.com>
2024-04-17 16:08:56 +0500

    second commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route(path: '/', name: 'home')]
     public function homepage(): Response
     {
-        return new Response('<strong>Hello<strong>');
+        $testvar = 10;
+        return $this->render('main/homepage.html.twig', [
+            'testvar' => $testvar
+        ]);
     }
 }
