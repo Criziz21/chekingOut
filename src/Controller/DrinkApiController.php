a6fb7cf7c1f599376032f59072df56f6898dcbc1
Criziz21 <xioamialisher777@gmail.com>
2024-04-18 00:13:54 +0500

    idk

--- src/Controller/DrinkApiController.php
+++ src/Controller/DrinkApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\Drink;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class DrinkApiController extends AbstractController
 {
     #[Route(path: '/api/drinks', name: 'getDrinks')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
+        // dd($logger);
+        $logger->info('Drinks collection retrieved!');
         $var = [
             new Drink(1, 'Gorrila', 'energy drink', '450', 'lowmid'),
             new Drink(2, 'Drive me', 'energy drink', '300', 'good'),
